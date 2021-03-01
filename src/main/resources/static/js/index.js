64e954a74fa3dfd1913a5beef687fe13ac5050f2
Pierre Leonard <pierre.leonard@digia.com>
2021-03-01 17:53:54 +0200

    Prettyfing the webpage

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $("#okbutton").click(function(){
         listTrainData();
-    })
+    });
+    $("#unitNumber").keypress(function(event){
+        if (event.keyCode === 13) {
+            $("#okbutton").click();
+        }
+    });
 })
 
 function listTrainData(){
@@ -12,7 +17,7 @@ function listTrainData(){
 }
 
 function buildTrainDataList(data, status){
-    var newHtml = "<table id='trainDataTable'><tr><th>Unit number</th><th>Timestamp</th><th>Topic</th></tr>"
+    var newHtml = "<table class='table table-striped' id='trainDataTable'><tr><th>Unit number</th><th>Timestamp</th><th>Topic</th></tr>"
     for(var i = 0 ; i < data.length ; i++){
         var trainItem = data[i];
         newHtml += "<tr><td>" + trainItem.unitNumber + "</td><td>" + trainItem.ekeDate + "</td><td>" + trainItem.topicPart + "</td></tr>";
